4680dcebc3480c6dd392293a79d2916801ff022f
Shon <nizamkhujaev@gmail.com>
2022-06-18 22:02:43 +0500

    add dragStartHandler functionality

--- src/app.ts
+++ src/app.ts
@@ -165,7 +165,8 @@ class ProjectItem extends Component<HTMLUListElement, HTMLLIElement> implements 
 
     @autoBind
     dragStartHandler(event: DragEvent) {
-        console.log(event)
+        event.dataTransfer!.setData('text/plain', this.project.id);
+        event.dataTransfer!.effectAllowed = 'move';
     }
 
     dragEndHandler(_: DragEvent) {
@@ -198,12 +199,17 @@ class ProjectList extends Component<HTMLDivElement, HTMLElement> implements Drag
     
     @autoBind
     
-    dragOverHandler(_: DragEvent) {
-        const listEl = this.element.querySelector('ul')!;
-        listEl.classList.add('droppable')
+    dragOverHandler(event: DragEvent) {
+        if(event.dataTransfer && event.dataTransfer.types[0] === 'text/plain') {
+            event.preventDefault();
+            const listEl = this.element.querySelector('ul')!;
+            listEl.classList.add('droppable')
+        }
     }
     
-    dropHandler(_: DragEvent) {}
+    dropHandler(event: DragEvent) {
+        console.log(event.dataTransfer!.getData('text/plain'))
+    }
     
     @autoBind
     dragLeaveHandler(_: DragEvent) {
